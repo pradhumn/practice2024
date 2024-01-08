d6c2e7731fd4d97120a03ac4c666d68dcddc9363
Pradhumn Sharma <Pradhumn.Sharma@OLRRETAIL.COM>
2024-01-08 18:10:24 +0530

    [PS] Trapping rain water prob

--- ArraysProb/src/com/git/practice/MainHandler.java
+++ ArraysProb/src/com/git/practice/MainHandler.java
@@ -7,9 +7,42 @@ public class MainHandler {
 		// int[] ary = { 1, 2, -3, 4, 5, -9, 9, -9, 8, 5 };
 		// System.out.println(maxSumSubarray(ary));
 
-		int[] ary = {1, 2, 1, 4 ,7 ,8};
-		System.out.println(containerWithMostWater(ary));
+		// int[] ary = {1, 2, 1, 4 ,7 ,8};
+		// System.out.println(containerWithMostWater(ary));
 
+		int[] ary = { 2, 0, 2 };
+		System.out.println(trappingRainWaterProb(ary));
+
+	}
+
+	private static int trappingRainWaterProb(int[] ary) {
+		int left = 0;
+		int right = ary.length - 1;
+		int leftMax = 0;
+		int rightMax = 0;
+		int ans = 0;
+
+		while (left < right) {
+			if (ary[left] <= ary[right]) {
+				if (ary[left] > leftMax)
+					leftMax = ary[left];
+				else
+					ans = ans + (leftMax - ary[left]);
+				left++;
+			}
+
+			else {
+				if (ary[right] > rightMax)
+					rightMax = ary[right];
+
+				else
+					ans = ans + (rightMax - ary[right]);
+				right--;
+
+			}
+
+		}
+		return ans;
 	}
 
 	private static int containerWithMostWater(int[] ary) {
@@ -18,7 +51,7 @@ public class MainHandler {
 
 		int ans = 0;
 		while (left < right) {
-			
+
 			int water = Math.min(ary[left], ary[right]) * (right - left);
 
 			if (water > ans)
