f8452ac44a310e6de60d5e0b03136961019b4b3a
Pradhumn Sharma <Pradhumn.Sharma@OLRRETAIL.COM>
2024-01-08 09:11:52 +0530

    [PS] container with most water

--- ArraysProb/src/com/git/practice/MainHandler.java
+++ ArraysProb/src/com/git/practice/MainHandler.java
@@ -4,8 +4,34 @@ public class MainHandler {
 
 	public static void main(String[] args) {
 
-		int[] ary = { 1, 2, -3, 4, 5, -9, 9, -9, 8, 5 };
-		System.out.println(maxSumSubarray(ary));
+		// int[] ary = { 1, 2, -3, 4, 5, -9, 9, -9, 8, 5 };
+		// System.out.println(maxSumSubarray(ary));
+
+		int[] ary = {1, 2, 1, 4 ,7 ,8};
+		System.out.println(containerWithMostWater(ary));
+
+	}
+
+	private static int containerWithMostWater(int[] ary) {
+		int left = 0;
+		int right = ary.length - 1;
+
+		int ans = 0;
+		while (left < right) {
+			
+			int water = Math.min(ary[left], ary[right]) * (right - left);
+
+			if (water > ans)
+				ans = water;
+
+			if (ary[left] < ary[right])
+				left++;
+			else
+				right--;
+
+		}
+
+		return ans;
 	}
 
 	public static int maxSumSubarray(int[] ary) {
